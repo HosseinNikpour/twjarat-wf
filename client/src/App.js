39a469b25a08203cd326ea58defa31af25fc3416
Mohammad Sadegh Khoeini <mohammadk@spotify.com>
2022-03-07 21:37:06 +0100

    fix: Remove comments and some empty lines from client

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
@@ -11,31 +10,9 @@ import User from './forms/user/index';
 
 import WfItem from './forms/wf_item/index';
 import Unit from './forms/unit/index';
-// import Request from './forms/request/index';
-// import RequestInquiryNew from './forms/request_inquiry/create';
-// import RequestInquiry from './forms/request_inquiry/index';
-// import TechnicalCommittee from './forms/technical_committee/index';
-// // import TechnicalCommitteeNew from './forms/technical_committee/create';
-// import TechnicalCommitteeMeeting from './forms/technical_committee_meeting/index';
-// import Approvals from './forms/approvals/index';
-// import Proposal from './forms/proposal/index';
-// import ContractDraft from './forms/contract_draft/index';
-// import Contract from './forms/contract/index';
-
-// import Invoice from './forms/invoice/index';
-// import InvoiceApprove from './forms/invoice_inquiry/index';
-// import InvoicePayment from './forms/invoice_payment/index';
-// import InvoiceLogistics from './forms/invoice_logistics/index';
-// import InvoiceSendToCompany from './forms/invoice_send_company/index';
-// import InvoiceBackFromCompany from './forms/invoice_back_company/index';
 
 import Report1 from './reports/report1';
 import Report2 from './reports/report2';
-// import ImageViewer from './reports/imageViewer';
-
-//import Permission from './forms/permission/index';
-//import CommingSoon from './forms/commingSoon';
-//import MainPage from './reports/dashboard';
 import Cardboard from './forms/cardboard/index';
 
 import './assets/css/antd.rtl.css';
@@ -49,8 +26,6 @@ import PrivateRoute from './components/PrivateRoute'
 
 
 function App() {
-  const [selectedNav, setSelectedNav] = useState(0);
-  const [subMenu, setSubMenu] = useState(0);
   const [currentUser, setCurrentUser] = useState({});
 
   useEffect(() => {
@@ -58,14 +33,14 @@ function App() {
     if (user) {
       setCurrentUser({ name: user.name, lastLoginDate: user.last_login, role_id: user.role_id ,unit_id:user.unit_id});
     }
+  }, []);
 
-
-  }, [])
   const signOut = () => {
     setCurrentUser({});
     localStorage.clear();
     window.location.reload();
   }
+
   const loginCallback = () => {
     const user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : undefined;
     if (user) {
@@ -73,17 +48,14 @@ function App() {
       setCurrentUser({ name: user.name, lastLoginDate: user.last_login, role_id: user.role });
     }
   }
+
   return (
     <div className="App" dir='RTL'>
-
       <Router>
-
         <div className="main-content" id="panel">
-
           <nav className="navbar navbar-top navbar-expand navbar-dark bg-primary border-bottom">
             <div className="container-fluid">
               <div className="collapse navbar-collapse" id="navbarSupportedContent">
-
                 <Link to="/" >
                   <img src={logo} className="navbar-brand-img" alt="..." style={{ width: '59px' }} />
                 </Link>
@@ -153,43 +125,21 @@ function App() {
             <Switch>
 
               <PrivateRoute path="/user" component={User} role={0} />
-              {/*<PrivateRoute path="/permission" component={Permission} role={10} />*/}
               <PrivateRoute path="/unit" component={Unit} role={0} />
 
               <PrivateRoute path="/wfitem" component={WfItem} role="" />
               <PrivateRoute path="/report1" component={Report1} role="" />
               <PrivateRoute path="/report2" component={Report2} role="" />
-              {/*<PrivateRoute path="/createReqInquiry" component={RequestInquiryNew} role="" />
-              <PrivateRoute path="/reqInquiry" component={RequestInquiry} role="" />
-              <PrivateRoute path="/technicalcommittee" component={TechnicalCommittee} role="" />
-              <PrivateRoute path="/createTechnicalcommittee" component={TechnicalCommitteeMeeting} role="" />
-              <PrivateRoute path="/approvals" component={Approvals} role="" />
-              <PrivateRoute path="/proposal" component={Proposal} role="" />
-              <PrivateRoute path="/contract_draft" component={ContractDraft} role="" />
-              <PrivateRoute path="/contract" component={Contract} role="" />
-              <PrivateRoute path="/invoice" component={Invoice} role="" />
-              <PrivateRoute path="/approveInvoice" component={InvoiceApprove} role="" />
-              <PrivateRoute path="/logisticsInvoice" component={InvoiceLogistics} role="" />
-              <PrivateRoute path="/paymentInvoice" component={InvoicePayment} role="" />
-              <PrivateRoute path="/toCompanyInvoice" component={InvoiceSendToCompany} role="" />
-              <PrivateRoute path="/companyBackInvoice" component={InvoiceBackFromCompany} role="" />
-
-              <PrivateRoute path="/rpttimeline" component={ReportTimeline} role="" />
-              <PrivateRoute path="/imgViewer" component={ImageViewer} role="" /> */}
-
-              {/* <Route path="/commingSoon" component={CommingSoon} role="" /> */}
 
               <Route path="/login">
                 <Login callback={loginCallback} />
               </Route>
               <PrivateRoute path="/" component={Cardboard} />
-
             </Switch>
           </div>
 
 
         </div>
-        {/* <div id="footer"> <a href="mailto: info@bstict.com">طراحی و پیاده سازی : BSTICT </a>  </div> */}
       </Router>
     </div>
   );

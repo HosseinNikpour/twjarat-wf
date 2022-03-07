0c3deef67bc63bfd62adbf27afe567ced4e70a57
Mohammad Sadegh Khoeini <mohammadk@spotify.com>
2022-03-07 21:42:36 +0100

    fix: Format with prettier

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import logo from './assets/img/brand/tejarat.png';
-import profile from './assets/img/brand/user.png';
-import logout from './assets/img/brand/logout.png';
-import Login from './components/login'
+import logo from "./assets/img/brand/tejarat.png";
+import profile from "./assets/img/brand/user.png";
+import logout from "./assets/img/brand/logout.png";
+import Login from "./components/login";
 
-import User from './forms/user/index';
+import User from "./forms/user/index";
 
-import WfItem from './forms/wf_item/index';
-import Unit from './forms/unit/index';
+import WfItem from "./forms/wf_item/index";
+import Unit from "./forms/unit/index";
 
-import Report1 from './reports/report1';
-import Report2 from './reports/report2';
-import Cardboard from './forms/cardboard/index';
+import Report1 from "./reports/report1";
+import Report2 from "./reports/report2";
+import Cardboard from "./forms/cardboard/index";
 
-import './assets/css/antd.rtl.css';
-import './assets/vendor/nucleo/css/nucleo.rtl.css';
-import './assets/vendor/@fortawesome/fontawesome-free/css/all.min.css';
-import './assets/css/argon.rtl.css';
-import './assets/fonts/IRANSans/style.css';
-import './assets/css/custom.css';
+import "./assets/css/antd.rtl.css";
+import "./assets/vendor/nucleo/css/nucleo.rtl.css";
+import "./assets/vendor/@fortawesome/fontawesome-free/css/all.min.css";
+import "./assets/css/argon.rtl.css";
+import "./assets/fonts/IRANSans/style.css";
+import "./assets/css/custom.css";
 
-import PrivateRoute from './components/PrivateRoute'
-
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   const [currentUser, setCurrentUser] = useState({});
 
   useEffect(() => {
-    const user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : undefined;
+    const user = localStorage.getItem("user")
+      ? JSON.parse(localStorage.getItem("user"))
+      : undefined;
     if (user) {
-      setCurrentUser({ name: user.name, lastLoginDate: user.last_login, role_id: user.role_id ,unit_id:user.unit_id});
+      setCurrentUser({
+        name: user.name,
+        lastLoginDate: user.last_login,
+        role_id: user.role_id,
+        unit_id: user.unit_id,
+      });
     }
   }, []);
 
@@ -39,47 +45,65 @@ function App() {
     setCurrentUser({});
     localStorage.clear();
     window.location.reload();
-  }
+  };
 
   const loginCallback = () => {
-    const user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : undefined;
+    const user = localStorage.getItem("user")
+      ? JSON.parse(localStorage.getItem("user"))
+      : undefined;
     if (user) {
       debugger;
-      setCurrentUser({ name: user.name, lastLoginDate: user.last_login, role_id: user.role });
+      setCurrentUser({
+        name: user.name,
+        lastLoginDate: user.last_login,
+        role_id: user.role,
+      });
     }
-  }
+  };
 
   return (
-    <div className="App" dir='RTL'>
+    <div className="App" dir="RTL">
       <Router>
         <div className="main-content" id="panel">
           <nav className="navbar navbar-top navbar-expand navbar-dark bg-primary border-bottom">
             <div className="container-fluid">
-              <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <Link to="/" >
-                  <img src={logo} className="navbar-brand-img" alt="..." style={{ width: '59px' }} />
+              <div
+                className="collapse navbar-collapse"
+                id="navbarSupportedContent"
+              >
+                <Link to="/">
+                  <img
+                    src={logo}
+                    className="navbar-brand-img"
+                    alt="..."
+                    style={{ width: "59px" }}
+                  />
                 </Link>
 
-                <div className="head-all" > جریان فرآیندی ابلاغ هدف گذاری های شعب </div>
+                <div className="head-all">
+                  {" "}
+                  جریان فرآیندی ابلاغ هدف گذاری های شعب{" "}
+                </div>
 
-                {localStorage.getItem('user') && <ul className="navbar-nav align-items-center  ml-md-auto login-container ">
-                  <li className="nav-item dropdown" >
-                    <span className="nav-link" >
-                      <div className='nav-login'>
-                        <img src={profile} className="profile" alt="..." />
-                        <span className="">{currentUser ? currentUser.name : ''}</span>
-                      </div>
-                    </span>
+                {localStorage.getItem("user") && (
+                  <ul className="navbar-nav align-items-center  ml-md-auto login-container ">
+                    <li className="nav-item dropdown">
+                      <span className="nav-link">
+                        <div className="nav-login">
+                          <img src={profile} className="profile" alt="..." />
+                          <span className="">
+                            {currentUser ? currentUser.name : ""}
+                          </span>
+                        </div>
+                      </span>
+                    </li>
+                    <div className="logoutBox" onClick={() => signOut()}>
+                      <img src={logout} className="logout" alt="..." />
 
-                  </li>
-                  <div className="logoutBox" onClick={() => signOut()}>
-                    <img src={logout} className="logout" alt="..." />
-
-                    <span className="exit" >خروج</span>
-                  </div>
-
-                </ul>}
-
+                      <span className="exit">خروج</span>
+                    </div>
+                  </ul>
+                )}
               </div>
             </div>
           </nav>
@@ -87,43 +111,38 @@ function App() {
             <div className="top-menu">
               <ul>
                 <li>
-                <Link to="/"><span>
-                    کارتابل
-                  </span> </Link>
-                </li>
-                <li>
-                  <Link to="/wfitem"><span>
-                    ایجاد گردش کار
-                  </span>
+                  <Link to="/">
+                    <span>کارتابل</span>{" "}
                   </Link>
                 </li>
                 <li>
-                <Link to="/report1"><span>
-                    گزارش گردش کار
-                  </span></Link>
+                  <Link to="/wfitem">
+                    <span>ایجاد گردش کار</span>
+                  </Link>
                 </li>
                 <li>
-                  <span>
-                    گزارش پیشرفت
-                  </span>
+                  <Link to="/report1">
+                    <span>گزارش گردش کار</span>
+                  </Link>
                 </li>
                 <li>
-                  <Link to="/unit"><span>
-                    واحد ها
-                  </span></Link>
-                  </li>
-                  <li>
-                  <Link to="/user"><span>
-                    مدیریت کاربران
-                  </span></Link>
+                  <span>گزارش پیشرفت</span>
+                </li>
+                <li>
+                  <Link to="/unit">
+                    <span>واحد ها</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/user">
+                    <span>مدیریت کاربران</span>
+                  </Link>
                 </li>
               </ul>
             </div>
           </div>
           <div className="header bg-light pb-6">
-
             <Switch>
-
               <PrivateRoute path="/user" component={User} role={0} />
               <PrivateRoute path="/unit" component={Unit} role={0} />
 
@@ -137,8 +156,6 @@ function App() {
               <PrivateRoute path="/" component={Cardboard} />
             </Switch>
           </div>
-
-
         </div>
       </Router>
     </div>
